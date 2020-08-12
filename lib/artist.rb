2d51c8e0ecb41d1d324106b6826bb12adfa50d63
Hannah Angle <hannahcangle@gmail.com>
2020-08-12 17:18:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -31,9 +31,9 @@ end
   def add_song(song)
     @songs << song
     song.artist = self
-  end
+  #end
 
-  def add_songs(songs)
+ # def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
 end
