4740637756916b15651b5a7d723416f5739fb109
Hannah Angle <hannahcangle@gmail.com>
2020-08-12 17:24:31 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,21 +1,17 @@
-module Memorable 
- module ClassMethods
-  
+module Memorable
+  module ClassMethods
     def reset_all
       self.all.clear
     end
-
+ 
     def count
       self.all.count
     end
+  end
  
- module InstanceMethods
-   
+  module InstanceMethods
     def initialize
       self.class.all << self
     end
   end
- 
-  end
-
-
+end
